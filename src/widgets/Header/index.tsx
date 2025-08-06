882c153dd76f5bc1f1a8b3f3625faa18bc02f11f
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-08-06 17:38:59 +0500

    fixed language changer

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -2,15 +2,19 @@ import { Link } from "react-router-dom";
 import { FaAngleDown, FaBars, FaGlobe } from "react-icons/fa";
 import styles from "./style.module.css";
 import { LangDropdown } from "./ui/LangDropdown";
+import { useState } from "react";
 
 export const Header = () => {
+  const [openLangDropdown, setOpenLangDropdown] = useState(false);
+  const closeDropdown = () => setOpenLangDropdown(false);
+
   return (
     <header className={`${styles.headerSection}`}>
       <div className={`container ${styles.headerCont}`}>
         <Link to={"/"} className={`${styles.logo}`}>
           1LOG
         </Link>
-        <div className={`${styles.headerMenu}`}>
+        <div className={`${styles.headerMenu} h-full`}>
           <div>
             <div className={`${styles.setMoney}`}>
               <div className={`${styles.headerSelect}`}>
@@ -21,15 +25,19 @@ export const Header = () => {
             </div>
           </div>
 
-          <div className="relative">
-            <div className={`${styles.setLang}`}>
+          <div className="relative h-full">
+            <div
+              onClick={() => setOpenLangDropdown(!openLangDropdown)}
+              onMouseLeave={closeDropdown}
+              className={`${styles.setLang} h-full flex items-center`}
+            >
               <div className={`${styles.headerSelect}`}>
                 <FaGlobe fontSize={20} />
                 <span>RU</span>
               </div>
               <div className={`${styles.setLangCont}`}></div>
+              <LangDropdown isOpen={openLangDropdown} onClose={closeDropdown} />
             </div>
-            <LangDropdown />
           </div>
 
           <div>
--- src/widgets/Header/ui/LangDropdown.tsx
+++ src/widgets/Header/ui/LangDropdown.tsx
@@ -1,26 +1,75 @@
+import { useTranslation } from "react-i18next";
 import flag_ru from "../../../assets/icons/flag_ru.svg";
 import flag_uz from "../../../assets/icons/flag_uz.svg";
 
-export const LangDropdown = () => {
+interface DropdownProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const LangDropdown: React.FC<DropdownProps> = ({ isOpen, onClose }) => {
+  const { i18n } = useTranslation();
+
+  const handleLanguageChange = (lng: string) => {
+    i18n.changeLanguage(lng);
+    onClose();
+  };
+
   return (
-    <div className="absolute left-1/2 -translate-x-1/2 w-16 bg-white text-black border border-gray-500 rounded p-1 shadow-xl">
-      <button className="flex items-center gap-x-2 py-0.5">
-        <div className="w-2/5 border-spacing-0.5 border-gray-300">
-          <img src={flag_ru} alt="Flag" className="w-full object-cover" />
+    <div
+      onMouseLeave={onClose}
+      className={`absolute top-[70%] left-1/2 -translate-x-1/2 overflow-hidden ${
+        isOpen ? "visible opacity-100" : "invisible opacity-0"
+      } transition-all duration-300 w-16 bg-white text-black border border-gray-500 rounded p-1 shadow-xl`}
+    >
+      <button
+        onClick={() => handleLanguageChange("ru")}
+        className="flex items-center gap-x-2 py-0.5 w-full"
+      >
+        <div className="w-2/5">
+          <img src={flag_ru} alt="Flag RU" className="w-full object-cover" />
         </div>
-        <span className="font-medium text-sm">RU</span>
+        <span
+          className={`font-medium text-sm ${
+            i18n.language === "ru" && "text-[#041e90]"
+          }`}
+        >
+          RU
+        </span>
       </button>
-      <button className="flex items-center gap-x-2 py-0.5">
-        <div className="w-2/5 border-spacing-0.5 border-gray-300">
-          <img src={flag_uz} alt="Flag" className="w-full object-cover" />
+      <button
+        onClick={() => handleLanguageChange("uz-Latn")}
+        className="flex items-center gap-x-2 py-0.5 w-full"
+      >
+        <div className="w-2/5">
+          <img src={flag_uz} alt="Flag UZ" className="w-full object-cover" />
         </div>
-        <span className="font-medium text-sm">UZ</span>
+        <span
+          className={`font-medium text-sm ${
+            i18n.language === "uz-Latn" && "text-[#041e90]"
+          }`}
+        >
+          UZ
+        </span>
       </button>
-      <button className="flex items-center gap-x-2 py-0.5">
-        <div className="w-2/5 border-spacing-0.5 border-gray-300">
-          <img src={flag_uz} alt="Flag" className="w-full object-cover" />
+      <button
+        onClick={() => handleLanguageChange("uz-Cyrl")}
+        className="flex items-center gap-x-2 py-0.5 w-full"
+      >
+        <div className="w-2/5">
+          <img
+            src={flag_uz}
+            alt="Flag UZ Cyr"
+            className="w-full object-cover"
+          />
         </div>
-        <span className="font-medium text-sm">УЗ</span>
+        <span
+          className={`font-medium text-sm ${
+            i18n.language === "uz-Cyrl" && "text-[#041e90]"
+          }`}
+        >
+          УЗ
+        </span>
       </button>
     </div>
   );
