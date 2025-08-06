3f11925b0c85043cc9483311dbbed9bdb36d414b
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-08-06 14:26:04 +0500

    webapp params test

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,3 +1,20 @@
+import { useParams } from "react-router-dom";
+
 export const Welcome = () => {
-  return <div>Welcome</div>;
+  const params = useParams();
+
+  return (
+    <div>
+      <div>
+        <span>
+          User id: <strong>{params.id}</strong>
+        </span>
+      </div>
+      <div>
+        <span>
+          Language: <strong>{params.lang}</strong>
+        </span>
+      </div>
+    </div>
+  );
 };
