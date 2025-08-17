8885a6771bde4c372402f2ae95d5c4e760b6596b
Sunnatbek Qadamov <118986314+sunnatbekweb@users.noreply.github.com>
2025-08-17 21:17:24 +0500

    Update index.tsx: PAGE_SIZE

--- src/widgets/Cargo/index.tsx
+++ src/widgets/Cargo/index.tsx
@@ -19,7 +19,7 @@ export interface RouteData {
   total: number;
 }
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 5;
 
 export const Cargo = () => {
   const filters = useSelector((state: RootState) => state.filters);
