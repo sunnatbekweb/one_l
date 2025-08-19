cdb20b84e6c2ee70f0353c417144a259e03c7994
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-08-19 16:00:24 +0500

    fix

--- src/entities/NotificationList/api/getNotificationsApi.ts
+++ src/entities/NotificationList/api/getNotificationsApi.ts
@@ -7,18 +7,5 @@ export const getNotifications = async () => {
     `${baseUrl}/user/notifications/${Cookies.get("user_id")}/`
   );
 
-  const savedKeys = Object.keys(Cookies.get()).filter((key) =>
-    key.startsWith("notify_")
-  );
-
-  if (!savedKeys.length) {
-    return [];
-  }
-
-  const filtered = response.data.filter((item: any) => {
-    const key = `notify_${item.origin}_${item.destination}`;
-    return savedKeys.includes(key);
-  });
-
-  return filtered;
+  return response.data;
 };
