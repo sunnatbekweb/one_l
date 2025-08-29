ec0c987faba36f7ba48e0e7f73a86a27ddfa2fb2
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-08-29 20:18:31 +0500

    date fix

--- src/shared/lib/formatDate.ts
+++ src/shared/lib/formatDate.ts
@@ -5,27 +5,53 @@ import {
   type Locale,
   parse,
 } from "date-fns";
-import { ru, uz, uzCyrl } from "date-fns/locale";
+import {
+  ru as ruLocale,
+  uz as uzLocale,
+  uzCyrl as uzCyrlLocale,
+} from "date-fns/locale";
 import i18n from "@/shared/config/i18n";
 
+const customRu: Locale = {
+  ...ruLocale,
+  formatDistance: (token, count, options) => {
+    const result = ruLocale.formatDistance!(token, count, options);
+    return result.replace("около ", "");
+  },
+};
+
+const customUz: Locale = {
+  ...uzLocale,
+  formatDistance: (token, count, options) => {
+    const result = uzLocale.formatDistance!(token, count, options);
+    return result.replace("tahminan ", "");
+  },
+};
+
+const customUzCyrl: Locale = {
+  ...uzCyrlLocale,
+  formatDistance: (token, count, options) => {
+    const result = uzCyrlLocale.formatDistance!(token, count, options);
+    return result.replace("тахминан ", "");
+  },
+};
+
 const localesMap: Record<string, Locale> = {
-  ru,
-  "uz-Latn": uz,
-  "uz-Cyrl": uzCyrl,
+  ru: customRu,
+  "uz-Latn": customUz,
+  "uz-Cyrl": customUzCyrl,
 };
 
 function getLocale(): Locale {
-  const locale = localesMap[i18n.language];
-  return locale || ru;
+  return localesMap[i18n.language] || customRu;
 }
 
-export function formatCustomDate(dateStr: string): string | undefined {
-  if (dateStr !== null) {
-    const parsed = parse(dateStr, "dd/MM/yyyy HH:mm", new Date());
-    return isValid(parsed)
-      ? format(parsed, "dd.MM - HH:mm", { locale: getLocale() })
-      : "";
-  }
+export function formatCustomDate(dateStr: string): string {
+  if (!dateStr) return "";
+  const parsed = parse(dateStr, "dd/MM/yyyy HH:mm", new Date());
+  return isValid(parsed)
+    ? format(parsed, "dd.MM - HH:mm", { locale: getLocale() })
+    : "";
 }
 
 export function formatRelativeDate(dateStr: string): string {
